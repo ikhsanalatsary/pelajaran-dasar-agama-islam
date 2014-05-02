8fc8cfc5b8db06ddd7c2c58d72413a61de958c50
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2014-05-02 20:39:00 +0700

    Menambahkan aqidah controller

--- js/controllers.js
+++ js/controllers.js
@@ -16,3 +16,31 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
+
+.controller('AqidahCtrl',
+	function($scope){
+	$scope.aqidah = [
+	  { title: 'Agama Islam', id: 1},
+	  { title: 'Rukun dan Makna Islam', id: 2},
+	  { title: 'Penjelasan Ringkas Rukun Islam 1', id: 3},
+	  { title: 'Penjelasan Ringkas Rukun Islam 2', id: 4},
+	  { title: 'Islam, Iman dan Ihsan', id: 5},
+	  { title: 'Agama Islam untuk Seluruh Manusia', id: 6},
+	  { title: 'Syarat Syahadat La ilaha illallah', id: 7},
+	  { title: 'Tiga Pokok Ajaran Islam', id: 8},
+	  { title: 'Empat Kaidah Utama Tauhid', id: 9},
+	  { title: 'Agar Ibadah Diterima di Sisis Allah', id: 10},
+	  { title: 'Awas Syirik!', id: 11},
+	  { title: 'Iman Kepada Allah', id: 12},
+	  { title: 'Iman Kepada Malaikat', id: 13},
+	  { title: 'Iman Kepada Kitab Allah', id: 14},
+	  { title: 'Iman Kepada Utusan Allah', id: 15},
+	  { title: 'Iman Kepada Hari Akhir', id: 16},
+	  { title: 'Iman Kepada Takdir', id: 17},
+	];
+})
+
+.controller('AkidahCtrl',
+	function($scope, $stateParams){
+	
+})
